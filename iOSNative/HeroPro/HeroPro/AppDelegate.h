f07fc9ede97d896db15e502cda53028610b268b1
KonradIT <mail@chernowii.com>
2014-01-04 17:50:27 +0100

    add android and iOS and blackberry native

--- iOSNative/HeroPro/HeroPro/AppDelegate.h
+++ iOSNative/HeroPro/HeroPro/AppDelegate.h
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface ViewController : UIViewController {
+    IBOutlet UIWebView *MyHTMLfile;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 
